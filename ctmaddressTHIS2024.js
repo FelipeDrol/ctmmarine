eb066f7ef18c3a8e4425b8fe616d86fcaa0a551a
FelipeDrol <felipe.grossi@outlook.com>
2024-08-14 04:54:19 -0300

    Add files via upload

---
+++ ctmaddressTHIS2024.js
@@ -0,0 +1,68 @@
+const ctm_stores = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -80.366726,
+          25.798836
+
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(305) 646-1339",
+        "phone": "3056461339",
+        "address": "2685 NW 105th Ave",
+        "city": "Doral",
+        "country": "United States",
+        "crossStreet": "at NW 27th St",
+        "postalCode": "33172",
+        "state": "FL",
+        "text": "Tripping Animals Brewing Co.",
+        "notice": "Call ahead to ensure they have what you are looking for."
+      }
+    },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.726396,
+    28.243072 ]
+   },
+   "properties": {
+
+
+     "address": "2250 Town Center Ave # 101",
+     "city": "Melbourne",
+     "country": "United States",
+     "postalCode": "32940",
+     "state": "FL",
+     "text": "28 North Gastropub",
+     "notice": "Call ahead to ensure they have what you are looking for."
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.142996,
+    26.057059 ]
+   },
+   "properties": {
+
+
+     "address": "236 N Federal Highwayunit# 104",
+     "city": "Dania Beach",
+     "country": "United States",
+     "postalCode": "33004",
+     "state": "FL",
+     "text": "3 Sons Brewing Co",
+     "notice": "Call ahead to ensure they have what you are looking for."
+    }
+  }
+  ]
+};
